eabffd9c2774460a43568db749fe94e7bb347f83
Warra <warren@wearenext.co.za>
2017-02-04 14:05:34 +0200

    Adds new website.

---
+++ resources/views/footer/scripts.blade.php
@@ -0,0 +1,3 @@
+@section("scripts")
+    <script type="text/javascript" src="materialize/js/materialize.min.js"/>
+@endsection
---
+++ resources/views/header/head.blade.php
@@ -0,0 +1,6 @@
+@section("head")
+<head>
+    <title>Just Grace</title>
+    <link rel="stylesheet" type="text/css" href="materialize/css/materialize.min.css"/>
+</head>
+@endsection
